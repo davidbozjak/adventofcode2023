afa7dd3adf2810696606d012e02f095a2b3a3eea
David Božjak <bozjak@gmail.com>
2023-11-24 18:19:14 +0100

    Set up 2023 dev env, fork of 2022

---
+++ SantasToolbox/WorldWithPath.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace SantasToolbox
+{
+    public class WorldWithPath<T> : IWorld
+        where T : IWorldObject
+    {
+        private readonly List<IWorldObject> worldObjects = new();
+        public IEnumerable<IWorldObject> WorldObjects => this.worldObjects.Cast<IWorldObject>();
+
+        public WorldWithPath(IWorld world, IEnumerable<T> path)
+        {
+            this.worldObjects.AddRange(world.WorldObjects);
+
+            this.worldObjects.AddRange(path.Select(w => new PathTile(w)));
+        }
+
+        private class PathTile : IWorldObject
+        {
+            private readonly IWorldObject pathObject;
+            public Point Position => pathObject.Position;
+
+            public char CharRepresentation => '*';
+
+            public int Z => int.MaxValue;
+
+            public PathTile(IWorldObject pathObject)
+            {
+                this.pathObject = pathObject;
+            }   
+        }
+    }
+}
